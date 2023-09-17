3d22c6dc70bc1a0b1d8687b8b7cb91e518c8bf09
TanyaMkrtchyan <tanymkrtchyan@gmail.com>
2023-09-17 23:35:30 +0400

    create modal component

---
+++ components/Modal.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-dialog
+    :value="isOpenModal"
+    persistent
+    max-width="290"
+  >
+    <v-card>
+      <v-card-title class="text-h6">
+        {{ title }}
+      </v-card-title>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          color="red darken-1"
+          text
+          @click="$emit('close')"
+        >
+          No
+        </v-btn>
+        <v-btn
+          color="green darken-1"
+          text
+          @click="$emit('submit')"
+        >
+          Yes
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: 'Modal',
+  props: {
+    isOpenModal: {
+      type: Boolean,
+      default: false
+    },
+    title: {
+      type: String,
+      default: 'Modal title'
+    }
+  }
+}
+</script>
+  
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,28 @@
 <template>
   <div>
-    {{counter}}
+    <!-- {{counter}} -->
 
-    <v-btn primary @click="submit">submit</v-btn>
+    <!-- <v-btn primary @click="submit">submit</v-btn> -->
+
+    <v-btn primary small @click="openModal">open modal</v-btn>
+    <Modal :is-open-modal="isOpenModal" @close="closeModal" @submit="submit" :title="title" />
   </div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex'
+import { mapGetters, mapActions, mapMutations } from 'vuex'
 
 export default {
   name: 'IndexPage',
+  data() {
+    return {
+      title: 'Delete your Account?'
+    }
+  },
   computed: {
     ...mapGetters({
-      counter: 'general/getCounter'
+      counter: 'general/getCounter',
+      isOpenModal: 'general/getModalState'
     })
   },
   methods: {
@@ -21,8 +30,22 @@ export default {
       increment: 'general/incrementCounter',
       setCounter: 'general/setCounter'
     }),
+    ...mapMutations({
+      setIsOpenModal: 'general/setIsOpenModal',
+      setModalTitle: 'general/setModalTitle'
+    }),
     submit() {
       this.setCounter(11)
+    },
+    openModal() {
+      this.setModalTitle(this.title)
+      this.setIsOpenModal(true)
+    },
+    closeModal() {
+      this.setIsOpenModal(false)
+    },
+    submit() {
+      this.setIsOpenModal(false)
     }
   }
 }
