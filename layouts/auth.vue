4862bcc5a3791d9a282c7bba120b06ddf597379e
TanyaMkrtchyan <tanymkrtchyan@gmail.com>
2023-10-06 13:33:23 +0400

    sign in/sign up UI

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,7 +1,16 @@
 <template>
   <v-app>
-    <v-container>
-      <Nuxt />
+    <v-container fluid class="pr-1 pl-0 py-0 fill-height">
+      <v-row align-content="stretch" class="fill-height">
+        <v-col lg="4" align-self="center">
+          <v-card class="card" color="transparent">
+            <Nuxt />
+          </v-card>
+        </v-col>
+        <v-col class="py-1 pl-1 d-none d-lg-block" lg="8">
+          <v-img src="/main-bg.jpg" class="rounded-xl" width="100%" height="100%" />
+        </v-col>
+      </v-row>
     </v-container>
   </v-app>
 </template>
@@ -10,3 +19,9 @@
 export default {
   name: 'AuthLayout',
 }
+</script>
+<style scoped>
+.v-sheet.v-card:not(.v-sheet--outlined) {
+  box-shadow: none;
+}
+</style>
--- pages/auth/sign-up.vue
+++ pages/auth/sign-up.vue
@@ -1,67 +1,70 @@
 <template>
-  <v-row class="d-flex justify-center align-center main-content">
-    <v-col cols="12" sm="4" md="4">
-      <v-card class="p-10">
-        <v-form v-model="valid" class="pa-4" @submit.prevent="submit">
-          <!-- <v-text-field
-            v-model="firstname"
-            label="First name"
-            required
-            :rules="rules.required"
-            outlined
-          ></v-text-field>
-          <v-text-field
-            v-model="lastname"
-            label="Last name"
-            required
-            :rules="rules.required"
-            outlined
-          ></v-text-field> -->
-          <v-text-field
-            v-model="email"
-            label="E-mail"
-            required
-            :rules="rules.email"
-            outlined
-          ></v-text-field>
-          <v-text-field
-            v-model="password"
-            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-            :type="showPassword ? 'text' : 'password'"
-            name="input-10-1"
-            label="Password"
-            @click:append="showPassword = !showPassword"
-            outlined
-            required
-            :rules="rules.password"
-          ></v-text-field>
-          <!-- <v-text-field
-            v-model="confirmPassword"
-            :append-icon="showConfirmPassword ? 'mdi-eye' : 'mdi-eye-off'"
-            :type="showConfirmPassword ? 'text' : 'password'"
-            name="input-10-1"
-            label="Password"
-            @click:append="showConfirmPassword = !showConfirmPassword"
-            outlined
-            required
-            :rules="[rePassword]"
-          ></v-text-field> -->
+  <div class="pa-4">
+    <h1>Welcome to TeamU</h1>
+    <p class="mb-12">Please enter your details</p>
+    <v-form v-model="valid" @submit.prevent="submit">
+      <!-- <v-text-field
+        v-model="firstname"
+        label="First name"
+        required
+        :rules="rules.required"
+        outlined
+      ></v-text-field>
+      <v-text-field
+        v-model="lastname"
+        label="Last name"
+        required
+        :rules="rules.required"
+        outlined
+      ></v-text-field> -->
+      <v-text-field
+        v-model="email"
+        label="E-mail"
+        required
+        :rules="rules.email"
+        outlined
+      ></v-text-field>
+      <v-text-field
+        v-model="password"
+        :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+        :type="showPassword ? 'text' : 'password'"
+        name="input-10-1"
+        label="Password"
+        @click:append="showPassword = !showPassword"
+        outlined
+        required
+        :rules="rules.password"
+      ></v-text-field>
+      <!-- <v-text-field
+        v-model="confirmPassword"
+        :append-icon="showConfirmPassword ? 'mdi-eye' : 'mdi-eye-off'"
+        :type="showConfirmPassword ? 'text' : 'password'"
+        name="input-10-1"
+        label="Password"
+        @click:append="showConfirmPassword = !showConfirmPassword"
+        outlined
+        required
+        :rules="[rePassword]"
+      ></v-text-field> -->
 
-          <v-btn
-            class="m-10"
-            color="primary"
-            block
-            large
-            :disabled="!valid"
-            type="submit"
-            :loading="loading"
-          >
-            submit
-          </v-btn>
-        </v-form>
-      </v-card>
-    </v-col>
-  </v-row>
+      <v-btn
+        class="m-10"
+        color="primary"
+        block
+        large
+        :disabled="!valid"
+        type="submit"
+        :loading="loading"
+      >
+        submit
+      </v-btn>
+
+      <p class="mt-6 text-center">
+        Already have an account?
+        <NuxtLink to="/auth/sign-in" class="font-weight-bold text-decoration-none">Sign in here</NuxtLink>
+      </p>
+    </v-form>
+  </div>
 </template>
 
 <script>
@@ -112,9 +115,3 @@ export default {
   },
 }
 </script>
-
-<style scoped>
-.main-content {
-  height: 100vh;
-}
-</style>
